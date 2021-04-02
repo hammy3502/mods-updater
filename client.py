c8eaa81768c4b68d406cd1afa31eddc761fac4ce
hammy3502 <hammy275@gmail.com>
2021-04-02 15:36:31 -0400

    Server Defines Version + Forge Download

--- client.py
+++ client.py
@@ -62,14 +62,16 @@ def full_path(path):
     return os.path.expandvars(os.path.expanduser(path))
 
 
-def download(down_url):
+def download(down_url, safe=False):
     try:
         r = requests.get(down_url, allow_redirects=True, timeout=30)
         if r.status_code != 200:
-            print("\nError " + str(r.status_code) + ".")
             raise Exception
         return r.content
     except Exception:
+        if safe:
+            return None
+        print("\nError " + str(r.status_code) + ".")
         print("\n"*5)
         print("Failed to download file!")
         input("Press ENTER to exit...")
@@ -79,16 +81,23 @@ def download(down_url):
 def main():
     print("Initiating...")
     # Set terminal size to 140, while keeping original height
-    os.system("mode con: cols=140 lines={}".format(os.get_terminal_size()[1]))
-    i = 9999
-    working_dir = ""
-    while i > 0:
-        working_dir = full_path("%appdata%\\.minecraft\\{}".format(modpack_folder) + str(i))
-        if os.path.isdir(working_dir):
-            break
-        else:
-            i -= 1
+    try:
+        os.system("mode con: cols=140 lines={}".format(os.get_terminal_size()[1]))
+    except OSError:
+        pass
+    i = download(url.format("version.txt"))
+    i = i.strip()
+    try:
+        i = int(i)
+    except TypeError:
+        print("Error determining version. Please yell at hammy3502!")
+        exit(1)
+    working_dir = full_path("%appdata%\\.minecraft\\{}".format(modpack_folder) + str(i))
     print("Using {}".format(modpack_folder) + str(i))
+    if not os.path.isdir(working_dir):
+        os.mkdir(working_dir)
+        os.chdir(working_dir)
+        os.mkdir("mods")
     os.chdir(os.path.join(working_dir, "mods"))
     print("Getting list of mods to delete...")
     to_del = download(url.format("to_delete_mods.txt")).decode("utf-8")
@@ -126,6 +135,15 @@ def main():
         progress(this_progress, "    Writing     {}...".format(n))
         open(n, 'wb').write(content)
         print(" "*139, end="\r")
+    print("Checking for new Forge version...")
+    forge_content = download(url.format("forge.jar"), True)
+    if forge_content is not None:
+        print("Running Forge installer. Please click install!")
+        os.chdir(os.path.expandvars("%temp%"))
+        with open("forge.jar", 'wb') as f:
+            f.write(forge_content)
+        os.system('"C:\\Program Files (x86)\\Minecraft Launcher\\runtime\\jre-x64\\bin\\javaw.exe" -jar forge.jar')
+
     print("\nSuccessfully downloaded all mods!")
 
 
--- setup_server.py
+++ setup_server.py
@@ -10,7 +10,7 @@ os.chdir("to_delete_mods")
 to_del = os.listdir()
 to_del_str = "\n".join(to_del)
 if to_del_str.strip() == "":
-    x = input("Delete all mods or no mods? [a/n]")
+    x = input("Delete all mods or no mods? [a/n] ")
     if x == "a":
         to_del_str = "all"
     elif x == "n":
@@ -18,6 +18,26 @@ if to_del_str.strip() == "":
     else:
         print("That isn't either or!")
         exit(1)
+
+os.chdir(os.path.expandvars(".."))
+if os.path.isfile("version.txt"):
+    with open("version.txt", 'r') as ver:
+        c_ver = ver.read().strip()
+else:
+    c_ver = -1
+
+new_ver = input("Input modpack version (ENTER to use previous version of {}) ".format(c_ver))
+if new_ver == "":
+    new_ver = c_ver
+else:
+    try:
+        new_ver = int(new_ver)
+    except TypeError:
+        print("Didn't specify a valid number!")
+        exit(1)
+
+print("Writing version to version file...")
+open("version.txt", 'w').write(str(new_ver))
 
 print("Creating mods to send list...")
 os.chdir(root)
